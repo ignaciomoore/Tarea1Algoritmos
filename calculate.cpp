30cbb369d86640453b0fcc4d36f3a67e64b31af5
Ignacio Moore <ignacio.moore@ug.uchile.cl>
2019-09-27 18:31:15 -0300

    Calculate limit block works

--- calculate.cpp
+++ calculate.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <windef.h>
 #include <algorithm>
+#include <cmath>
 
 /**
  * Calculates the pending block on the bottom right corner
@@ -63,6 +64,70 @@ int* calculate_block(int block_size, int* left_block, int* top_block, int* diago
             Y_char_index++;
         }
     }
+    return result_block;
+}
+
+/**
+ * Calculates the values of a block on the limit of the matrix
+ *
+ * @param block_size
+ * @param left_block
+ * @param top_block
+ * @param diagonal_block
+ * @param string_size
+ * @param X
+ * @param Y
+ * @param Y_char_index
+ *      The y axis matrix coordinate in which the block begins
+ * @param X_char_index
+ *      The x axis matrix coordinate in which the block begins
+ * @return
+ *      The int array of the values inside the block
+ */
+int* calculate_limit_block(int block_size, int* left_block, int* top_block, int* diagonal_block, int string_size, char* X,
+                           char* Y, int Y_char_index, int X_char_index) {
+
+    int blocks_per_line = ceil((double) string_size / block_size);
+    int number_of_values = string_size % (blocks_per_line - 1);
+    int* result_block = new int[block_size];
+
+    int top_value;
+    int left_value;
+    int diagonal_value;
+    int first_minimum_value;
+    int second_minimum_value;
+
+    for (int w = 0; w < number_of_values; w++) {
+        if (w == 0) {
+            if (X[X_char_index] == Y[Y_char_index]) {
+                result_block[0] = diagonal_block[block_size - 1];
+            } else {
+                top_value = top_block[0];
+                left_value = left_block[block_size - 1];
+                first_minimum_value = std::min(top_value + 1, left_value + 1);
+                diagonal_value = diagonal_block[block_size - 1];
+                second_minimum_value = std::min(first_minimum_value, diagonal_value + 1);
+                result_block[0] = second_minimum_value;
+            }
+        } else {
+            if (X[X_char_index] == Y[Y_char_index]) {
+                result_block[w] = top_block[w - 1];
+            } else {
+                top_value = top_block[w];
+                left_value = result_block[w - 1];
+                first_minimum_value = std::min(top_value + 1, left_value + 1);
+                diagonal_value = top_block[w - 1];
+                second_minimum_value = std::min(first_minimum_value, diagonal_value + 1);
+                result_block[w] = second_minimum_value;
+            }
+        }
+        X_char_index++;
+        if (X_char_index >= string_size) {
+            X_char_index = 0;
+            Y_char_index++;
+        }
+    }
+
     return result_block;
 }
 
--- main.cpp
+++ main.cpp
@@ -12,56 +12,20 @@ using namespace std;
 
 int main() {
 
-    int step = 1;
-    int block_size = 3;    //  JUST FOR TESTING
-    int* top_block;
-    int* left_block;
-    int* diagonal_block;
-    int* current_block;
-    string X;
-    string Y;
+    int left[] = {2,1,2};
+    int diagonal[] = {1,2,2};
+    int top[] = {3};
 
-    string filename = "two_strings.txt";    //  JUST FOR TESTING
+    char x[] = " ananas";
+    char y[] = " banana";
 
-    pair<string, string> XeY = get_strings(filename);
-    X = XeY.first;
-    Y = XeY.second;
+    int jj = 6;
+    int ii = 5;
 
-    int string_length = X.length();
-    int blocks_per_line = ceil((double) string_length/block_size);
-    int total_steps = string_length * blocks_per_line;
+    int* r = new int[3];
+    r = calculate_limit_block(3,left,top,diagonal,7,x,y,ii,jj);
 
-    for (int i = 0; i < string_length; i++) {
-        for (int j = 0; j < blocks_per_line; j++) {
-            if (i == 0) {
-                for (int k = 0; k < block_size; k++) {
-                    current_block[k + j] = k + j;
-                }
-                write_block(current_block, block_size, step);
-                step++;
-            }
-            else if (j == 0) {
-                top_block = read_block(get_top_step(step, blocks_per_line), block_size);
-                for (int k = 0; k < block_size; k++) {
-                    if (k == 0)
-                        current_block[k] = calculate_Y_coordinate(step, block_size, string_length);
-                    else {
-                        int x_index = calculate_X_coordinate(step, block_size, string_length);
-                        int y_index = calculate_Y_coordinate(step, block_size, string_length);
-                        if (X[x_index] == Y[y_index])
-                            current_block[k] = top_block[k - 1];
-                        else {
-                            int minimum = min(top_block[k] + 1, current_block[k - 1] + 1);
-                            minimum = min(minimum, top_block[k - 1] + 1);
-                            current_block[k] = minimum;
-                        }
-                    }
-                }
-                step++;
-            }
-            //  DO STUFF HERE
-        }
-    }
+    cout << r[0];
 
     return 0;
 }
